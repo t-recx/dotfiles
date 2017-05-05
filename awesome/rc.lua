5ee4b83e1d5e449029879165fa5e3a28a7e3e7be
João Bruno <bruno.joao@live.com.pt>
2017-05-05 02:19:54 +0100

    Mouse wheel no longer changes workspaces

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -61,9 +61,7 @@ taglist.buttons = awful.util.table.join(
 awful.button({ }, 1, awful.tag.viewonly),
 awful.button({ modkey }, 1, awful.client.movetotag),
 awful.button({ }, 3, awful.tag.viewtoggle),
-awful.button({ modkey }, 3, awful.client.toggletag),
-awful.button({ }, 4, awful.tag.viewnext),
-awful.button({ }, 5, awful.tag.viewprev)
+awful.button({ modkey }, 3, awful.client.toggletag)
 )
 
 tasklist = {}
@@ -134,9 +132,7 @@ for s = 1, screen.count() do
 end
 
 root.buttons(awful.util.table.join(
-awful.button({ }, 3, function () main_menu:toggle() end),
-awful.button({ }, 4, awful.tag.viewnext),
-awful.button({ }, 5, awful.tag.viewprev)
+awful.button({ }, 3, function () main_menu:toggle() end)
 ))
 
 globalkeys = awful.util.table.join(
